663944fc3b52c4a6606aead02598f2444db1c84f
Ihsan Arif Rahman <ihsan_arif@apps.ipb.ac.id>
2017-02-26 01:18:36 +0700

    initial AdminLTE template

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,8 +2,14 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data['main'] = 'back/home/index';
+        $data['menu'] = '0';
+
+        $data['css']=array('css/datatables.min');
+        $data['js']= array('js/jquery.dataTables','js/dataTables.bootstrap');
+		$this->load->view('back/layouts/master',$data);
 	}
 }

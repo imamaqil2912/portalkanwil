65e2a928f24827879b9deef4990484742a7e1c1f
imamaqil2912 <imamaqilrabbani1@gmail.com>
2017-02-26 10:31:00 +0700

    Add Administrator Sidebar Menu

--- application/views/back/layouts/menu.php
+++ application/views/back/layouts/menu.php
@@ -19,66 +19,82 @@
 </form>
 <!-- /.search form -->
 <!-- sidebar menu: : style can be found in sidebar.less -->
-<ul class="sidebar-menu">
-<li class="header">MAIN NAVIGATION</li>
+
 <?php
 /// menu untuk administrator
 if($this->ion_auth->is_admin())
 {
 ?>
+<ul class="sidebar-menu">
+<li class="header">MENU ADMINISTRATOR</li>
     <li class="active">
         <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Beranda</span>
+            <i class="fa fa-home"></i> <span>Beranda</span>
         </a>
     </li>
     <li class="treeview">
         <a href="#">
-        <i class="fa fa-files-o"></i>
+        <i class="fa fa-wrench"></i>
         <span>Setting</span>
         <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
         </span>
         </a>
         <ul class="treeview-menu">
-            <li><a href="#"><i class="fa fa-circle-o"></i> Sinkronasi Data STKAA</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Administrasi User</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Administrasi Link</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Administrasi Pengumuman</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Administrasi Software & Driver</a></li>
+            <li><a href="#"><i class="fa fa-group"></i> Sinkronasi Data SIKKA</a></li>
+            <li><a href="#"><i class="fa fa-users"></i> Administrasi User</a></li>
+            <li><a href="#"><i class="fa fa-link"></i> Administrasi Link</a></li>
+            <li><a href="#"><i class="fa fa-file-sound-o"></i> Administrasi Pengumuman</a></li>
+            <li><a href="#"><i class="fa fa-file"></i> Administrasi Software & Driver</a></li>
+        </ul>
+    </li>
+        <li class="treeview">
+        <a href="#">
+        <i class="fa fa-refresh"></i>
+        <span>Update Data</span>
+        <span class="pull-right-container">
+            <span class="fa fa-angle-left pull-right"></span>
+        </span>
+        </a>
+        <ul class="treeview-menu">
+        <li><a href="pages/layout/top-nav.html"><i class="fa fa-line-chart"></i>Update Data Penerimaan</a></li>
+        <li><a href="pages/layout/boxed.html"><i class="fa fa-street-view"></i>Update Data Kepatuhan</a></li>
+        <li><a href="pages/layout/fixed.html"><i class="fa  fa-file-text-o"></i>Update Data STP</a></li>
+        <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-check-square-o"></i> Update Data IKU</a></li>
+        </ul>
+    </li>
+        <li class="treeview">
+        <a href="#">
+        <i class="fa fa-exchange"></i>
+        <span>Monitoring</span>
+        <span class="pull-right-container">
+            <span class="fa fa-angle-left pull-right"></span>
+        </span>
+        </a>
+        <ul class="treeview-menu">
+        <li><a href="pages/layout/top-nav.html"><i class="fa fa-bar-chart"></i>Pengawasan Rutin</a></li>
+        <li><a href="pages/layout/boxed.html"><i class="fa fa-pie-chart"></i>Pengawasan Non Rutin</a></li>
+        <li><a href="pages/layout/fixed.html"><i class="fa fa-tags"></i>Pengawasan Data Eksternal</a></li>
         </ul>
     </li>
 <?php
 }else{
-    /// menu untuk users
+    
 ?>
-    <li class="active treeview">
+<li class="treeview">
         <a href="#">
-        <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+        <i class="fa fa-exchange"></i>
+        <span>Monitoring</span>
         <span class="pull-right-container">
-            <i class="fa fa-angle-left pull-right"></i>
+            <span class="fa fa-angle-left pull-right"></span>
         </span>
         </a>
         <ul class="treeview-menu">
-        <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-        <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
-        </ul>
-    </li>
-    <li class="treeview">
-        <a href="#">
-        <i class="fa fa-files-o"></i>
-        <span>Layout Options</span>
-        <span class="pull-right-container">
-            <span class="label label-primary pull-right">4</span>
-        </span>
-        </a>
-        <ul class="treeview-menu">
-        <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-        <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
-        <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
-        <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
+        <li><a href="pages/layout/top-nav.html"><i class="fa fa-bar-chart"></i>Pengawasan Rutin</a></li>
+        <li><a href="pages/layout/boxed.html"><i class="fa fa-pie-chart"></i>Pengawasan Non Rutin</a></li>
+        <li><a href="pages/layout/fixed.html"><i class="fa fa-tags"></i>Pengawasan Data Eksternal</a></li>
         </ul>
     </li>
 <?php
-}
 ?>
 </ul>

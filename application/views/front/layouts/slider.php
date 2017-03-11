5374221da27ae0fcc9dd41740858593f53ec6edb
doang1992 <doang.muhammad0@gmail.com>
2017-03-11 12:38:35 +0700

    style font home page, benerin gambar di dropdown carousel panel useful links

--- application/views/front/layouts/slider.php
+++ application/views/front/layouts/slider.php
@@ -9,19 +9,19 @@
 					<img src="<?php echo base_url() ?>assets/front/img/slides/nivo/imgta.jpg" data-thumb="<?php echo base_url() ?>assets/front/img/slides/nivo/imgta.jpg" alt="" title="#htmlcaption3" />
 				</div>
 				<div id="htmlcaption1" class="nivo-html-caption">
-					<h2>Selamat Datang di Portal Kanwil Jakut</h2>
+					<h2><b>Selamat Datang di Portal Kanwil Jakut</b></h2>
 					<p>
 						 Selamat datang di Portal Kanwil DJP Jakarta Utara. Dengan tampilan baru, kini portal lebih dinamis mengikuti perkembangan teknologi. Penguatan integrasi database membuat data yang ada pada Portal menjadi semakin baik.
 					</p>
 				</div>
 				<div id="htmlcaption2" class="nivo-html-caption">
-					<h2>Rapat Kordinasi Gabungan</h2>
+					<h2><b>Rapat Kordinasi Gabungan</b></h2>
 					<p>
 						 Sinergi dan Komunikasi, Kunci Pengamanan Penerimaan Pajak
 					</p>
 				</div>
 				<div id="htmlcaption3" class="nivo-html-caption">
-					<h2>Periode Terakhir Amnesti Pajak</h2>
+					<h2><b>Periode Terakhir Amnesti Pajak</b></h2>
 					<p>
 						 Program Amnesti Pajak Memasuki tahap akhir periode. Tetap Semangat Satu Jiwa!
 					</p>
